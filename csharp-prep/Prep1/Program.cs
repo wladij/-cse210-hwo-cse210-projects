8f4fb551b74863fd22d939702941eea22460436b
wladij <joew.candelario@gmail.com>
2022-09-14 16:49:12 -0500

    Done changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,7 +6,13 @@ namespace Prep1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is prep 1");
+            Console.WriteLine("What is your first name? " );
+            string name = Console.Readline();
+            Console.WriteLine("What is your last name? " );
+            string lastname = Console.Readline();
+            
+            Console.WriteLine();
+            Console.WriteLine($"Your name is "{lastname}", "{name}" "{lastname}".");
 
             // Write your code here
         }
