1b43c963829c6abee402d30c83fae5636570f430
wladij <joew.candelario@gmail.com>
2022-09-17 18:01:45 -0500

    completed all

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,7 +6,29 @@ namespace Prep3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Prep 3");
+             Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+          
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
         }
-    }
-}
+}   }
