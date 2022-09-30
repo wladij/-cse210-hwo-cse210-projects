9fc1d40a499719c374b6269b5f37a95913b5444f
wladij <joew.candelario@gmail.com>
2022-09-30 00:10:56 -0600

    completed

--- articulate/Unit02/Game/Die.cs
+++ articulate/Unit02/Game/Die.cs
@@ -1,31 +1,93 @@
 using System;
-
+using System.Collections.Generic;
 
 namespace Unit02.Game
 {
-    // TODO: Implement the Die class as follows...
-    // 1) Add the class declaration. Use the following class comment.
+     /// <summary>
+    /// A person who directs the game. 
+    ///
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </summary>
+    public class Director
+    {
+        List<Die> _dice = new List<Die>();
+        bool _isPlaying = true;
+        int _score = 0;
+        int _totalScore = 0;
 
         /// <summary>
-        /// A small cube with a different number of spots on each of its six sides.
-        /// 
-        /// The responsibility of Die is to keep track of its currently rolled value and the points its
-        /// worth.
-        /// </summary> 
-
-
-    // 2) Create the class constructor. Use the following method comment.
-
-        /// <summary>
-        /// Constructs a new instance of Die.
+        /// Constructs a new instance of Director.
         /// </summary>
+        public Director()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Die die = new Die();
+                _dice.Add(die);
+            }
+        }
 
-    
-    // 3) Create the Roll() method. Use the following method comment.
-        
         /// <summary>
-        /// Generates a new random value and calculates the points for the die. Fives are 
-        /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
+        /// Starts the game by running the main game loop.
         /// </summary>
-        
+        public void StartGame()
+        {
+            while (_isPlaying)
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+            }
+        }
+
+        /// <summary>
+        /// Asks the user if they want to roll.
+        /// </summary>
+        public void GetInputs()
+        {
+            Console.Write("Roll dice? [y/n] ");
+            string rollDice = Console.ReadLine();
+            _isPlaying = (rollDice == "y");
+        }
+
+        /// <summary>
+        /// Updates the player's score.
+        /// </summary>
+        public void DoUpdates()
+        {
+            if (!_isPlaying)
+            {
+                return;
+            }
+
+            _score = 0;
+            foreach (Die die in _dice)
+            {
+                die.Roll();
+                _score += die._points;
+            }
+            _totalScore += _score;
+        }
+
+        /// <summary>
+        /// Displays the dice and the score. Also asks the player if they want to roll again. 
+        /// </summary>
+        public void DoOutputs()
+        {
+            if (!_isPlaying)
+            {
+                return;
+            }
+
+            string values = "";
+            foreach (Die die in _dice)
+            {
+                values += $"{die._value} ";
+            }
+
+            Console.WriteLine($"You rolled: {values}");
+            Console.WriteLine($"Your score is: {_totalScore}\n");
+            _isPlaying = (_score > 0);
+        }
+    }
 }
